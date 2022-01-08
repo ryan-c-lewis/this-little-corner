f26a353d61e8a219d9f5b84ed44de89d358c680f
ryan-c-lewis <ryan.cp.lewis@protonmail.com>
2022-01-08 00:07:35 -0600

    link to video with timestamp

--- search-app/components/searchkit/Hits.jsx
+++ search-app/components/searchkit/Hits.jsx
@@ -10,18 +10,26 @@ export const HitsList = ({ data }) => (
             <EuiFlexItem grow={4}>
               <EuiTitle size="xs">
                 <div>
-                  <a href={hit.fields.url}><h6>{hit.fields.title}</h6></a>
-                  <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + hit.fields.id}
-                          title="YouTube video player" frameBorder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowFullScreen />
-                  <div>
-                    {hit.fields.transcript_parts.map((part) => (
-                        <span>{part.text} </span>
-                    ))}
-                  </div>
+                  <a href={hit.fields.url}><h4>{hit.fields.title}</h4></a>
+                  <h6>{hit.fields.date}</h6>
                 </div>
               </EuiTitle>
+              <EuiFlexGroup>
+                <EuiFlexItem grow={4}>
+                  <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + hit.fields.id}
+                        title="YouTube video player" frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen />
+                </EuiFlexItem>
+                <EuiFlexItem grow={4}>
+                  <div style={{'max-height': 315, 'overflow-y': 'auto'}}>
+                    <span>{hit.fields.description} </span>
+                    {hit.fields.transcript_parts.map((part) => (
+                        <a href={hit.fields.url + "#t=" + part.start + "s"}><span>{part.text} </span></a>
+                    ))}
+                  </div>
+                </EuiFlexItem>
+              </EuiFlexGroup>
             </EuiFlexItem>
           </EuiFlexGroup>
         </EuiFlexItem>
