fed533036721aeb2ac3e6c04e92775fa3165ba06
ryan-c-lewis <ryan.cp.lewis@protonmail.com>
2022-01-08 12:08:42 -0600

    control videos by links

--- search-app/components/searchkit/Hits.jsx
+++ search-app/components/searchkit/Hits.jsx
@@ -16,16 +16,16 @@ export const HitsList = ({ data }) => (
               </EuiTitle>
               <EuiFlexGroup>
                 <EuiFlexItem grow={4}>
-                  <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + hit.fields.id}
+                  <iframe id={"video_" + hit.fields.id} width="560" height="315" src={"https://www.youtube.com/embed/" + hit.fields.id + "?enablejsapi=1"}
                         title="YouTube video player" frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen />
                 </EuiFlexItem>
                 <EuiFlexItem grow={4}>
-                  <div style={{'max-height': 315, 'overflow-y': 'auto'}}>
+                  <div style={{'maxHeight': 315, 'overflowY': 'auto'}}>
                     <span>{hit.fields.description} </span>
                     {hit.fields.transcript_parts.map((part) => (
-                        <a href={hit.fields.url + "#t=" + part.start + "s"}><span>{part.text} </span></a>
+                        <a href={"javascript:jump('video_" + hit.fields.id + "', " + part.start + ");"} key={part.start}><span>{part.text} </span></a>
                     ))}
                   </div>
                 </EuiFlexItem>
--- search-app/hocs/withApollo.js
+++ search-app/hocs/withApollo.js
@@ -12,14 +12,43 @@ export default withApollo(
   },
   {
     render: ({ Page, props }) => {
+
+        function postMessageToPlayer(iframe, func, args) {
+            iframe.contentWindow.postMessage(JSON.stringify({
+                'event': 'command',
+                'func': func,
+                'args': args || []
+            }), '*');
+        }
+        function pauseAllExcept(iframeToIgnore) {
+            var i, frames;
+            frames = document.getElementsByTagName("iframe");
+            for (i = 0; i < frames.length; ++i) {
+                if (frames[i] === iframeToIgnore)
+                    continue;
+                postMessageToPlayer(frames[i], "pauseVideo", []);
+            }
+        }
+        function jump(id, time) {
+            let iframe = document.getElementById(id);
+            pauseAllExcept(iframe);
+            postMessageToPlayer(iframe, "seekTo", [time, true]);
+            postMessageToPlayer(iframe, "playVideo", []);
+        }
+        function setupCallPlayer() {
+            window['jump'] = jump;
+        }
+        
       return (
         <div>
             <Helmet>
                 <title>This Little Corner Searcher</title>
+                <script src="https://www.youtube.com/player_api"></script>
             </Helmet>
             <ApolloProvider client={props.apollo}>
               <Page {...props} />
             </ApolloProvider>
+            {setupCallPlayer()}
         </div>
       );
     }
