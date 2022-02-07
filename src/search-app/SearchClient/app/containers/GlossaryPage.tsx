5a931e8eeb4de8efd4b82ee8926cee9e007ada9d
ryan-c-lewis <ryan.cp.lewis@protonmail.com>
2022-02-06 21:51:35 -0600

    start filling out glossary page

--- src/search-app/SearchClient/app/containers/GlossaryPage.tsx
+++ src/search-app/SearchClient/app/containers/GlossaryPage.tsx
@@ -8,7 +8,8 @@ import {
   EuiPageContent,
   EuiPageContentBody,
   EuiSpacer,
-  EuiTitle
+  EuiTitle,
+  EuiAccordion, EuiPagination
 } from "@elastic/eui";
 
 interface IGlossaryAppProps {
@@ -20,6 +21,29 @@ export default class GlossaryPage extends React.Component<IGlossaryAppProps, {}>
 
   render() {
     const { appState } = this.props;
+
+    const Term = (inputs: any) => (
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiAccordion id={'video_' + inputs.title} buttonContent={inputs.title} >
+              <div>
+                <EuiFlexGroup>
+                  {inputs.videos.map(x => (
+                    <EuiFlexItem>
+                      <div className="video-container">
+                        <iframe src={'https://www.youtube.com/embed/' + x}
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen />
+                      </div>
+                    </EuiFlexItem>
+                  ))}
+                </EuiFlexGroup>
+              </div>
+            </EuiAccordion>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+    )
 
     return (
         <EuiPage>
@@ -36,7 +60,23 @@ export default class GlossaryPage extends React.Component<IGlossaryAppProps, {}>
                 <EuiSpacer />
                 <EuiFlexGroup>
                   <EuiFlexItem>
-                    Note: This page will be improved soon. For now, here's links to some common topics:
+                    <EuiTitle>
+                      <h1>Important Terms</h1>
+                    </EuiTitle>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+                <Term title={"4 Kinds of Knowing ("} videos={['Gyx5tyFttfA']}/>
+                <Term title={"Agent and Arena"} videos={['yy47YzvGniQ?start=1824', 'Q8NrCR4ifoo?start=1148']}/>
+                <Term title={"Combinatorial Explosion"} videos={['6V1eMvGGcXQ?start=529', 'JZogi3h3pbA?start=1159', '9j5O-tnaFzE']}/>
+                <Term title={"Dialogos"} videos={['FdZlQYtIJ0Q?start=4417', 'bPy6W-c5_9Y?start=1474']}/>
+                <Term title={"God #1 and God #2"} videos={['G19xovk0F0s?start=2587', 'U0VqRxNRyyI?start=266']} />
+                <EuiSpacer />
+                <EuiSpacer />
+                <EuiFlexGroup>
+                  <EuiFlexItem>
+                    <EuiTitle>
+                      <h1>All Terms</h1>
+                    </EuiTitle>
                   </EuiFlexItem>
                 </EuiFlexGroup>
                 {appState.topics.map((topic) => (
